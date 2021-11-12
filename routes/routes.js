297a973aec276112c037289a85e5e8cc3cd611db
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-11-12 22:23:52 +0800

    added login page functionality

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,24 @@
 const express = require('express');
-
 const controller = require('../controllers/controller.js');
-
+const bcrypt = require('bcrypt');
+const Employee = require('../models/employee.js');
 const app = express.Router();
 
 // login
 app.get('/', controller.getIndex);
+
+app.post('/home', async function (req, res) {
+    const { username, password } = req.body;
+    const user = await Employee.findOne({
+        username: username,
+    });
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+        res.render('event-tracker-home');
+    } else {
+        res.send('Not registered');
+    }
+});
 
 // event-tracker
 // app.get('/event-tracker/home', eventController.getHome);
