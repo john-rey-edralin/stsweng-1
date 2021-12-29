f2ad977f47f1814f56bda800f9ed315b9bd4cdf7
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-12-29 16:16:29 +0800

    updated employee schema

--- models/employee.js
+++ models/employee.js
@@ -4,6 +4,11 @@ const { Schema } = mongoose;
 const employeeSchema = new Schema({
     username: String,
     password: String,
+    hasAccess: Boolean,
+    role: {
+        type: String,
+        enum: ['admin', 'employee'],
+    },
 });
 
 module.exports = mongoose.model('employee', employeeSchema);
