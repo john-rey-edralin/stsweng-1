619358bedde31e3db1c9b6c8188f33326b7221cc
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-12-29 16:16:51 +0800

    Update dummy-employee.js

--- dummy-employee.js
+++ dummy-employee.js
@@ -7,12 +7,37 @@ const bcrypt = require('bcrypt');
 const Employee = require('./models/employee.js');
 const saltRounds = 10;
 
-const password = 'admin';
+let password = 'admin';
+
+//admin
+bcrypt
+    .hash(password, saltRounds)
+    .then((hash) => {
+        Employee.create({
+            username: 'admin',
+            password: hash,
+            role: 'admin',
+            hasAccess: true,
+        }).then(() => {
+            console.log('employee created');
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+//employee
+password = 'employee';
 
 bcrypt
     .hash(password, saltRounds)
     .then((hash) => {
-        Employee.create({ username: 'admin', password: hash }).then(() => {
+        Employee.create({
+            username: 'employee',
+            password: hash,
+            role: 'employee',
+            hasAccess: true,
+        }).then(() => {
             console.log('employee created');
         });
     })
