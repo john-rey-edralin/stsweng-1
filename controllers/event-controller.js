c9ab89da286184231e681c1ff61b18df78a3a870
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2022-01-03 22:24:59 +0800

    Reordered functions

--- controllers/event-controller.js
+++ controllers/event-controller.js
@@ -66,6 +66,32 @@ const eventController = {
         });
     },
 
+    putReservations: async function (req, res) {
+        const { id, data } = req.body;
+        const _id = mongoose.Types.ObjectId(id);
+
+        const doc = await Event.findOneAndUpdate(
+            { _id, status: 'reserved' },
+            data,
+            { returnDocument: 'after' }
+        );
+
+        res.send(doc);
+    },
+
+    putPencilbookings: async function (req, res) {
+        const { id, data } = req.body;
+        const _id = mongoose.Types.ObjectId(id);
+
+        const doc = await Event.findOneAndUpdate(
+            { _id, status: 'booked' },
+            data,
+            { returnDocument: 'after' }
+        );
+
+        res.send(doc);
+    },
+
     getPencilBookings: async function (req, res) {
         const bookings = await Event.aggregate([
             { $match: { status: 'booked' } },
@@ -182,19 +208,6 @@ const eventController = {
         };
 
         res.render('event-tracker-pencilbookings', data);
-    },
-
-    putPencilbookings: async function (req, res) {
-        const { id, data } = req.body;
-        const _id = mongoose.Types.ObjectId(id);
-
-        const doc = await Event.findOneAndUpdate(
-            { _id, status: 'booked' },
-            data,
-            { returnDocument: 'after' }
-        );
-
-        res.send(doc);
     },
 
     getReservations: async function (req, res) {
@@ -316,18 +329,7 @@ const eventController = {
         res.render('event-tracker-reservations', data);
     },
 
-    putReservations: async function (req, res) {
-        const { id, data } = req.body;
-        const _id = mongoose.Types.ObjectId(id);
-
-        const doc = await Event.findOneAndUpdate(
-            { _id, status: 'reserved' },
-            data,
-            { returnDocument: 'after' }
-        );
-
-        res.send(doc);
-    },
+    
 
     getFood: function (req, res) {
         let projection = 'name price';
--- routes/routes.js
+++ routes/routes.js
@@ -8,46 +8,29 @@ const app = express.Router();
 // login
 app.get('/', controller.getIndex);
 
-// event-tracker
+// event-tracker home
 app.get('/event-tracker/home', eventController.getHome);
 
+// event-tracker form
 app.get('/event-tracker/create', eventController.getCreateEvent);
 app.post('/event-tracker/submit', eventController.postCreateEvent);
+app.get('/event-tracker/edit/:id', eventController.getEditEvent);
+
+// event-tracker form data retrieval
 app.get('/event-tracker/get/food', eventController.getFood);
 app.get('/event-tracker/get/event', eventController.getEvent);
 app.get('/event-tracker/get/charges', eventController.getCharges);
 app.get('/event-tracker/get/packages', eventController.getPackages);
-app.get(
-    '/event-tracker/check/event-availability',
-    eventController.getCheckEventAvailability
-);
+app.get('/event-tracker/check/event-availability', eventController.getCheckEventAvailability);
 
-app.route('/event-tracker/pencilbookings')
-    .get(eventController.getPencilBookings)
-    .put(eventController.putPencilbookings);
+// event-tracker pencilbooking list
+app.route('/event-tracker/pencilbookings').get(eventController.getPencilBookings).put(eventController.putPencilbookings);
+app.get('/event-tracker/pencilbookings/search', eventController.getPencilBookingsSearch);
+app.get('/event-tracker/pencilbookings/filter', eventController.getPencilBookingsFilter);
 
-app.post('/event-tracker/submit', eventController.postCreateEvent);
-app.get(
-    '/event-tracker/pencilbookings/search',
-    eventController.getPencilBookingsSearch
-);
-app.get(
-    '/event-tracker/pencilbookings/filter',
-    eventController.getPencilBookingsFilter
-);
+// event-tracker reservation list
+app.get('/event-tracker/reservations', eventController.getReservations);
+app.get('/event-tracker/reservations/search', eventController.getReservationsSearch);
+app.get('/event-tracker/reservations/filter', eventController.getReservationsFilter);
 
-app.route('/event-tracker/reservations')
-    .get(eventController.getReservations)
-    .put(eventController.putReservations);
-
-app.get('/event-tracker/edit/:id', eventController.getEditEvent);
-
-app.get(
-    '/event-tracker/reservations/search',
-    eventController.getReservationsSearch
-);
-app.get(
-    '/event-tracker/reservations/filter',
-    eventController.getReservationsFilter
-);
 module.exports = app;
