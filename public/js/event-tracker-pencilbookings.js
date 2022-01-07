6293a81fbcf8dcc2a5e63fa2e372b7b56d4595f9
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2022-01-07 21:27:05 +0800

    Added cancel pencilbookings function

--- public/js/event-tracker-pencilbookings.js
+++ public/js/event-tracker-pencilbookings.js
@@ -6,3 +6,17 @@ $(document).ready(function () {
     $('[data-toggle="popover"]').popover();
 });
 
+function deleteReservation(id) {
+    let json = JSON.stringify({
+        id: id,
+    });
+    $.ajax({
+        type: 'PUT',
+        url: '/event-tracker/reservations/cancel',
+        data: json,
+        contentType: 'application/json',
+        success: function (result) {
+            window.location.href = '/event-tracker/cancelled';
+        },
+    });
+}
