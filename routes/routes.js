7360cbe78804799f64b0495b06fe98238e4ac1a2
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2022-01-08 17:05:23 +0800

    Added sort and filter to cancelled events list

--- routes/routes.js
+++ routes/routes.js
@@ -36,5 +36,7 @@ app.get('/event-tracker/reservations/filter', eventController.getReservationsFil
 
 // event-tracker cancelled events list
 app.get('/event-tracker/cancelled', eventController.getCancelledEvents);
+app.get('/event-tracker/cancelled/search', eventController.getCancelledEventsSearch);
+app.get('/event-tracker/cancelled/filter', eventController.getCancelledEventsFilter);
 
 module.exports = app;
