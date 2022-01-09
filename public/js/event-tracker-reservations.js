182bfe268e71b70c55a0de8fb549e4e9bc13d7af
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2022-01-09 21:49:25 +0800

    Added js for marking event as done

--- public/js/event-tracker-reservations.js
+++ public/js/event-tracker-reservations.js
@@ -5,3 +5,18 @@ $(document).ready(function () {
 
     $('[data-toggle="popover"]').popover();
 });
+
+function finishEvent(id) {
+    let json = JSON.stringify({
+        id: id,
+    });
+    $.ajax({
+        type: 'PUT',
+        url: '/event-tracker/finish',
+        data: json,
+        contentType: 'application/json',
+        success: function (result) {
+            window.location.href = '/event-tracker/pastevents';
+        },
+    });
+}
