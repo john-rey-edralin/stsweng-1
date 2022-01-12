39abc70878c98fb0198121d0037e4ff41eb41bb7
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2022-01-12 10:49:21 +0800

    Updated calendar design

--- views/hbs-helper.js
+++ views/hbs-helper.js
@@ -38,3 +38,25 @@ hbs.registerHelper('ifEquals', function (arg1, arg2, options) {
 hbs.registerHelper('multiply', function (a, b) {
     return Number(a) * Number(b);
 });
+
+hbs.registerHelper('getPrevMonth', function (date) {
+    let current;
+    if (date.getMonth() == 0) {
+        current = new Date(date.getFullYear() - 1, 11, 1);
+    } else {
+        current = new Date(date.getFullYear(), date.getMonth() - 1, 1);
+    }
+    return `${(current.getMonth() + 1).toString().padStart(2, 0)}-${current.getFullYear()}`;
+});
+
+//returns the next month
+hbs.registerHelper('getNextMonth', function (date) {
+    let current;
+    if (date.getMonth() == 11) {
+        current = new Date(date.getFullYear() + 1, 0, 1);
+    } else {
+        current = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+    }
+
+    return `${(current.getMonth() + 1).toString().padStart(2, 0)}-${current.getFullYear()}`;
+});
