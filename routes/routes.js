d171a014b752725aea1e743529c59442a8ebbc49
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2022-01-15 18:48:48 +0800

    added get recent events

--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,23 @@ const express = require('express');
 
 const controller = require('../controllers/controller.js');
 const eventController = require('../controllers/event-controller.js');
-
+const adminController = require('../controllers/admin-controller.js');
 const app = express.Router();
 
 // login
 app.get('/', controller.getIndex);
+
+// admin
+// app.get('/admin', adminController.getAdminHome);
+// app.get('/admin/register', adminController.getRegisterEmployee);
+// app.post('/admin/register', adminController.postRegisterEmployee);
+// app.get('/admin/employee', adminController.getAllEmployees);
+// app.get('/admin/employee/current', adminController.getAllCurrentEmployees);
+// app.get('/admin/employee/former', adminController.getAllFormerEmployees);
+// app.get('/admin/employee/:id', adminController.getEmployee);
+// app.put('/admin/employee/:username', adminController.putEmployeeInfo);
+// app.get('/admin/activity/:username', adminController.getEmployeeActivity);
+app.get('/admin/activity/recent', adminController.getRecentActivity);
 
 // event-tracker home
 app.get('/event-tracker/home', eventController.getHome);
