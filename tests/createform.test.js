529a6ca23c632fffa2e65edd689e2d759c28d758
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2022-01-31 18:09:04 +0800

    added test of aj

--- tests/createform.test.js
+++ tests/createform.test.js
@@ -1,277 +1,294 @@
-//require('dotenv').config();
-//const db = require('../models/db.js');
-const { validDate, validName, validNumber, validEventType, validEventTime, validPaxNum, validVenue, validPackage, eventAvailability } = require('../tests/form-functions.js');
+const {
+    getDateTime,
+    checkStringInput,
+    validDate,
+    validName,
+    validNumber,
+    validEventType,
+    validEventTime,
+    validPaxNum,
+    validVenue,
+    validPackage,
+    eventAvailability,
+} = require('../tests/form-functions.js');
 
 // Test Suite #1
-describe("Date", () => {
+describe('Date', () => {
+    // Unit Test #1
+    it('Before minimum date', () => {
+        const testVal = '2020-01-01';
 
-	// Unit Test #1
-	it("Before minimum date", () => {
-		const testVal = "2020-01-01";
+        const resVal = validDate(testVal, '2022-02-01');
 
-		const resVal = validDate(testVal);
+        expect(resVal).toEqual(false);
+    });
 
-		expect(resVal).toEqual(false);
-	});
+    // Unit Test #2
+    it('After maximum date', () => {
+        const testVal = '2040-01-01';
 
-	// Unit Test #2
-	it("After maximum date", () => {
-		const testVal = "2040-01-01";
+        const resVal = validDate(testVal, '2022-02-01');
 
-		const resVal = validDate(testVal);
+        expect(resVal).toEqual(false);
+    });
 
-		expect(resVal).toEqual(false);
-	});
+    // Unit Test #3
+    it('No date', () => {
+        const testVal = '';
 
-	// Unit Test #3
-	it("No date", () => {
-		const testVal = "";
+        const resVal = validDate(testVal, '2022-02-01');
 
-		const resVal = validDate(testVal);
+        expect(resVal).toEqual(false);
+    });
 
-		expect(resVal).toEqual(false);
-	});
+    // Unit Test #4
+    it('Valid date', () => {
+        const testVal = '2022-02-02';
 
-	// Unit Test #4
-	it("Valid date", () => {
-		const testVal = "2022-02-02";
+        const resVal = validDate(testVal, '2022-02-01');
 
-		const resVal = validDate(testVal);
-
-		expect(resVal).toEqual(true);
-	});
-
+        expect(resVal).toEqual(true);
+    });
 });
 
 // Test Suite #2
-describe("Name", () => {
+describe('Name', () => {
+    // Unit Test #1
+    it('No input', () => {
+        const testVal = '';
 
-	// Unit Test #1
-	it("No input", () => {
-		const testVal = "";
+        const resVal = validName(testVal);
 
-		const resVal = validName(testVal);
+        expect(resVal).toEqual(false);
+    });
 
-		expect(resVal).toEqual(false);
-	});
+    // Unit Test #2
+    it('Invalid name', () => {
+        const testVal = 'Bin;';
 
-	// Unit Test #2
-	it("Invalid name", () => {
-		const testVal = "Bin;";
+        const resVal = validName(testVal);
 
-		const resVal = validName(testVal);
+        expect(resVal).toEqual(false);
+    });
 
-		expect(resVal).toEqual(false);
-	});
+    // Unit Test #3
+    it('Valid name', () => {
+        const testVal = 'Bin';
 
-	// Unit Test #3
-	it("Valid name", () => {
-		const testVal = "Bin";
+        const resVal = validName(testVal);
 
-		const resVal = ValidName(testVal);
-
-		expect(resVal).toEqual(true);
-	});
+        expect(resVal).toEqual(true);
+    });
 });
 
 // Test Suite #3
-describe("Contact Number", () => {
+describe('Contact Number', () => {
+    // Unit Test #1
+    it('No input', () => {
+        const testVal = '';
 
-	// Unit Test #1
-	it("No input", () => {
-		const testVal = "";
+        const resVal = validNumber(testVal);
 
-		const resVal = validNumber(testVal);
+        expect(resVal).toEqual(false);
+    });
 
-		expect(resVal).toEqual(false);
-	});
+    // Unit Test #2
+    it('Invalid number', () => {
+        const testVal = '999999';
 
-	// Unit Test #2
-	it("Invalid number", () => {
-		const testVal = "999999";
+        const resVal = validNumber(testVal);
 
-		const resVal = validNumber(testVal);
+        expect(resVal).toEqual(false);
+    });
 
-		expect(resVal).toEqual(false);
-	});
+    // Unit Test #3
+    it('Valid number', () => {
+        const testVal = '09183774544';
 
-	// Unit Test #3
-	it("Valid number", () => {
-		const testVal = "09183774544";
+        const resVal = validNumber(testVal);
 
-		const resVal = ValidNumber(testVal);
-
-		expect(resVal).toEqual(true);
-	});
+        expect(resVal).toEqual(true);
+    });
 });
 
 // Test Suite #4
-describe("Event Type", () => {
+describe('Event Type', () => {
+    // Unit Test #1
+    it('No input', () => {
+        const testVal = '';
 
-	// Unit Test #1
-	it("No input", () => {
-		const testVal = "";
+        const resVal = validEventType(testVal);
 
-		const resVal = validEventType(testVal);
+        expect(resVal).toEqual(false);
+    });
 
-		expect(resVal).toEqual(false);
-	});
+    // Unit Test #2
+    it('Valid event type', () => {
+        const testVal = 'Birthday';
 
-	// Unit Test #2
-	it("Valid event type", () => {
-		const testVal = "Birthday";
+        const resVal = validEventType(testVal);
 
-		const resVal = validEventType(testVal);
-
-		expect(resVal).toEqual(false);
-	});
+        expect(resVal).toEqual(true);
+    });
 });
 
 // Test Suite #5
-describe("Event Time", () => {
+describe('Event Time', () => {
+    // Unit Test #1
+    it('No input', () => {
+        const testVal = '';
 
-	// Unit Test #1
-	it("No input", () => {
-		const testVal = "";
+        const resVal = validEventTime(testVal);
 
-		const resVal = validEventTime(testVal);
+        expect(resVal).toEqual(false);
+    });
 
-		expect(resVal).toEqual(false);
-	});
+    // Unit Test #2
+    it('Valid event time', () => {
+        const testVal = 'Afternoon (11:00 AM - 2:00 PM)';
 
-	// Unit Test #2
-	it("Valid event time", () => {
-		const testVal = "Afternoon (11:00 AM - 2:00 PM)";
+        const resVal = validEventTime(testVal);
 
-		const resVal = validEventTime(testVal);
-
-		expect(resVal).toEqual(true);
-	});
+        expect(resVal).toEqual(true);
+    });
 });
 
 // Test Suite #6
-describe("Number of Pax", () => {
+describe('Number of Pax', () => {
+    // Unit Test #1
+    it('Zero', () => {
+        const testVal = 0;
 
-	// Unit Test #1
-	it("Zero", () => {
-		const testVal = 0;
+        const resVal = validPaxNum(testVal);
 
-		const resVal = validPaxNum(testVal);
+        expect(resVal).toEqual(false);
+    });
 
-		expect(resVal).toEqual(false);
-	});
+    // Unit Test #2
+    it('Negative number', () => {
+        const testVal = -1;
 
-	// Unit Test #2
-	it("Negative number", () => {
-		const testVal = -1;
+        const resVal = validPaxNum(testVal);
 
-		const resVal = validPaxNum(testVal);
+        expect(resVal).toEqual(false);
+    });
 
-		expect(resVal).toEqual(false);
-	});
+    // Unit Test #3
+    it('Greater than the maximum (120)', () => {
+        const testVal = 121;
 
-	// Unit Test #3
-	it("Greater than the maximum (120)", () => {
-		const testVal = 121;
+        const resVal = validPaxNum(testVal);
 
-		const resVal = validPaxNum(testVal);
+        expect(resVal).toEqual(false);
+    });
 
-		expect(resVal).toEqual(false);
-	});
+    // Unit Test #4
+    it('Valid input', () => {
+        const testVal = 40;
 
-	// Unit Test #4
-	it("Valid input", () => {
-		const testVal = 40;
+        const resVal = validPaxNum(testVal);
 
-		const resVal = validPaxNum(testVal);
-
-		expect(resVal).toEqual(true);
-	});
+        expect(resVal).toEqual(true);
+    });
 });
 
 // Test Suite #7
-describe("Event Venue", () => {
+describe('Event Venue', () => {
+    // Unit Test #1
+    it('Selected none', () => {
+        const testVal = 0;
 
-	// Unit Test #1
-	it("Selected none", () => {
-		const testVal = 0;
+        const resVal = validVenue(testVal);
 
-		const resVal = validVenue(testVal);
+        expect(resVal).toEqual(true);
+    });
 
-		expect(resVal).toEqual(false);
-	});
+    // Unit Test #2
+    it('Selected one venue', () => {
+        const testVal = 1;
 
-	// Unit Test #2
-	it("Selected one venue", () => {
-		const testVal = 1;
+        const resVal = validVenue(testVal);
 
-		const resVal = validVenue(testVal);
+        expect(resVal).toEqual(true);
+    });
 
-		expect(resVal).toEqual(true);
-	});
+    // Unit Test #3
+    it('Selected more than one venue', () => {
+        const testVal = 2;
 
-	// Unit Test #3
-	it("Selected more than one venue", () => {
-		const testVal = 2;
+        const resVal = validVenue(testVal);
 
-		const resVal = validVenue(testVal);
+        expect(resVal).toEqual(true);
+    });
 
-		expect(resVal).toEqual(true);
-	});
+    // Unit Test #4
+    it('Selected all venues', () => {
+        const testVal = 3;
 
-	// Unit Test #4
-	it("Selected all venues", () => {
-		const testVal = 3;
+        const resVal = validVenue(testVal);
 
-		const resVal = validVenue(testVal);
-
-		expect(resVal).toEqual(true);
-	});
+        expect(resVal).toEqual(true);
+    });
 });
 
 // Test Suite #8
-describe("Event Package", () => {
+describe('Event Package', () => {
+    // Unit Test #1
+    it('Selected none', () => {
+        const testValGarden = 0;
+        const testValSunroom = 0;
+        const testValTerrace = 0;
 
-	// Unit Test #1
-	it("Selected none", () => {
-		const testValGarden = 0;
-		const testValSunroom = 0;
-		const testValTerrace = 0;
+        const resVal = validVenue(
+            testValGarden,
+            testValSunroom,
+            testValTerrace
+        );
 
-		const resVal = validVenue(testValGarden, testValSunroom, testValTerrace);
+        expect(resVal).toEqual(true);
+    });
 
-		expect(resVal).toEqual(false);
-	});
+    // Unit Test #2
+    it('Selected one venue', () => {
+        const testValGarden = 1;
+        const testValSunroom = 0;
+        const testValTerrace = 0;
 
-	// Unit Test #2
-	it("Selected one venue", () => {
-		const testValGarden = 1;
-		const testValSunroom = 0;
-		const testValTerrace = 0;
+        const resVal = validVenue(
+            testValGarden,
+            testValSunroom,
+            testValTerrace
+        );
 
-		const resVal = validVenue(testValGarden, testValSunroom, testValTerrace);
+        expect(resVal).toEqual(true);
+    });
 
-		expect(resVal).toEqual(true);
-	});
+    // Unit Test #3
+    it('Selected more than one venue', () => {
+        const testValGarden = 1;
+        const testValSunroom = 1;
+        const testValTerrace = 0;
 
-	// Unit Test #3
-	it("Selected more than one venue", () => {
-		const testValGarden = 1;
-		const testValSunroom = 1;
-		const testValTerrace = 0;
+        const resVal = validVenue(
+            testValGarden,
+            testValSunroom,
+            testValTerrace
+        );
 
-		const resVal = validVenue(testValGarden, testValSunroom, testValTerrace);
+        expect(resVal).toEqual(true);
+    });
 
-		expect(resVal).toEqual(true);
-	});
+    // Unit Test #4
+    it('Selected all venues', () => {
+        const testValGarden = 1;
+        const testValSunroom = 1;
+        const testValTerrace = 1;
 
-	// Unit Test #4
-	it("Selected all venues", () => {
-		const testValGarden = 1;
-		const testValSunroom = 1;
-		const testValTerrace = 1;
-
-		const resVal = validVenue(testValGarden, testValSunroom, testValTerrace);
-		expect(resVal).toEqual(true);
-	});
+        const resVal = validVenue(
+            testValGarden,
+            testValSunroom,
+            testValTerrace
+        );
+        expect(resVal).toEqual(true);
+    });
 });
--- tests/form-functions.js
+++ tests/form-functions.js
@@ -1,135 +1,163 @@
+const validator = require('validator');
+
+const getDateTime = (input) => {
+    var day = parseInt(input.slice(8, 10));
+    var month = parseInt(input.slice(5, 7));
+    var year = parseInt(input.slice(0, 4));
+
+    return new Date(year, month, day).getTime();
+};
+
+const checkStringInput = (input) => {
+    const blacklist = [
+        '~',
+        '`',
+        '!',
+        '@',
+        '#',
+        '$',
+        '%',
+        '^',
+        '&',
+        '*',
+        '(',
+        ')',
+        '_',
+        '=',
+        '+',
+        '{',
+        '}',
+        '[',
+        ']',
+        '|',
+        '\\',
+        ';',
+        ':',
+        "'",
+        '"',
+        '.',
+        ',',
+        '<',
+        '>',
+        '/',
+        '?',
+    ];
+    var flag = false;
+
+    for (const item of blacklist) if (input.indexOf(item) != -1) flag = true;
+
+    return flag;
+};
+
 const form_functions = {
-
-    getDateTime: (input) => {
-        var day = parseInt(input.slice(8, 10));
-        var month = parseInt(input.slice(5, 7));
-        var year = parseInt(input.slice(0, 4));
-
-        return new Date(year, month, day).getTime();
-    },
-
-    checkStringInput: (input) => {
-        const blacklist = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
-            "_", "=", "+", "{", "}", "[", "]", "|", "\\", ";", ":", "\'",
-            "\"", ".", ",", "<", ">", "/", "?"];
-        var flag = false;
-
-        for (const item of blacklist)
-            if (input.indexOf(item) != -1)
-                flag = true;
-
-        return flag;
-    },
+    getDateTime: getDateTime,
 
     // Event Details
     validDate: (dateInput, dateMin) => {
         // dateMin - date today
-        var dateMax = "2032-01-01";
+        var dateMax = '2032-01-01';
 
         var max = getDateTime(dateMax);
         var min = getDateTime(dateMin);
         var input = getDateTime(dateInput);
 
-        if (dateInput.length > 10)
-            return false;
+        if (dateInput.length > 10) return false;
         else {
-            if (dateInput == "")
-                return false;
-            else if ((input - min < 0) || isNaN(input))
-                return false;
-            else if ((input - max >= 0) || isNaN(input))
-                return false;
-            else //true = valid date
-                return true;
+            if (dateInput == '') return false;
+            else if (input - min < 0 || isNaN(input)) return false;
+            else if (input - max >= 0 || isNaN(input)) return false;
+            //true = valid date
+            else return true;
         }
     },
 
     validName: (nameInput) => {
         var name = validator.trim(nameInput);
-        if (validator.isEmpty(name))
-            return false;
-        else if (checkStringInput(name))
-            return false;
-        else
-            return true;
+        if (validator.isEmpty(name)) return false;
+        else if (checkStringInput(name)) return false;
+        else return true;
     },
 
     validNumber: (numInput) => {
-        if (validator.isEmpty(numInput))
-            return false;
-        else if (numInput.intlTelInput('isValidNumber'))
-            return true;
-        else
-            return false;
+        if (validator.isEmpty(numInput)) return false;
+        else if (numInput.length === 11) return true;
+        else return false;
     },
 
     validEventType: (eTypeInput) => {
         var eventtype = validator.trim(eTypeInput);
-        if (validator.isEmpty(eventtype))
-            return false;
+        if (validator.isEmpty(eventtype)) return false;
         return true;
     },
 
     validEventTime: (eTimeInput) => {
-        if (validator.isEmpty(eTimeInput))
-            return false;
+        if (validator.isEmpty(eTimeInput)) return false;
         return true;
     },
 
     validPaxNum: (paxNumInput) => {
-        if (paxNumInput < 0)
-            return false;
-        else if (paxNumInput == 0)
-            return false;
-        else if (paxNumInput > 120)
-            return false;
+        if (paxNumInput < 0) return false;
+        else if (paxNumInput == 0) return false;
+        else if (paxNumInput > 120) return false;
         return true;
     },
 
     validVenue: (venue) => {
         var checked = venue.length;
-        if (checked <= 0)
-            return false;
-        else
-            return true;
+        if (checked <= 0) return false;
+        else return true;
     },
 
     validPackage: (garden, sunroom, terrace) => {
-        let eventpackage = (garden || sunroom || terrace);
-        if (eventpackage == 0)
-            return false;
-        else
-            return true;
+        let eventpackage = garden || sunroom || terrace;
+        if (eventpackage == 0) return false;
+        else return true;
     },
 
-    eventAvailability: (date, time, garden_options, sunroom_options, terrace_options, checkedvenues) => {
-        if (date != '' && time != '' &&
-            (garden_options != '' || sunroom_options != '' || terrace_options != '')) {
+    eventAvailability: (
+        date,
+        time,
+        garden_options,
+        sunroom_options,
+        terrace_options,
+        checkedvenues
+    ) => {
+        if (
+            date != '' &&
+            time != '' &&
+            (garden_options != '' ||
+                sunroom_options != '' ||
+                terrace_options != '')
+        ) {
             let eventVenues = [];
 
             for (var i = 0; i < checkedvenues.length; i++) {
-                if (checkedvenues[i] == 'Garden' ||
+                if (
+                    checkedvenues[i] == 'Garden' ||
                     checkedvenues[i] == 'Sunroom' ||
-                    checkedvenues[i] == 'Terrace') {
+                    checkedvenues[i] == 'Terrace'
+                ) {
                     eventVenues.push(checkedvenues[i]);
                 }
-            };
+            }
 
             let data = {
                 status: 'reserved',
                 eventDate: date,
                 eventTime: time,
-                eventVenues: eventVenues
-            }
+                eventVenues: eventVenues,
+            };
 
-            $.get('/event-tracker/check/event-availability', data, function (result) {
-                if (result)
-                    return 'Unavailable';
-                else
-                    return 'Available';
-            });
+            $.get(
+                '/event-tracker/check/event-availability',
+                data,
+                function (result) {
+                    if (result) return 'Unavailable';
+                    else return 'Available';
+                }
+            );
         }
-    }
-}
+    },
+};
 
 module.exports = form_functions;
